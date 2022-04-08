6d0c65ab613de61a13960c04430c4fb064746f69
Artand <martinchandra99@gmail.com>
2022-04-08 09:45:39 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produk', function () {
+    return 'Halaman Katalog Produk';
+});
+
+Route::get('/profil', function () {
+    return 'Halaman Profil';
+});
+
+Route::get('/daftarpembeli', function () {
+    return 'Halaman Daftar Akun Pembeli';
+});
