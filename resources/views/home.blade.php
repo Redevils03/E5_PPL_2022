8da0814f5c76c14d65d21af85e7699be36e1fcf5
Artand <martinchandra99@gmail.com>
2022-04-08 13:39:43 +0700

    test1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
     <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
     <title>Kedelai Kamila, Penyedia Olahan Kedelai Online</title>
 </head>
 <body>
-    <h6 style="color: rgba(54, 211, 154, 1)">Kedelai Kamila</h6>
-{{-- <script src=""></script> --}}
+    <h4 class="kedelai">Kedelai Kamila</h4>
+    <div class="rectangle1">
+        <a href="#"><img class="home1" src="img/home.png"></a>
+        <a href="#"><img class="user1" src="img/user.png"></a>
+    </div>
+    <h1 class="title"><b>Beli Produk Olahan Kedelai Online?</b></h1>
 </body>
 </html>
