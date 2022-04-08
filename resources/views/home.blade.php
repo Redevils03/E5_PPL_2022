b1a9e1c4dd29614cac04f0280adc9f711b2ad7cc
Artand <martinchandra99@gmail.com>
2022-04-08 10:59:32 +0700

    warna dasar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
+    <title>Kedelai Kamila, Penyedia Olahan Kedelai Online</title>
+</head>
+<body>
+    <h6 style="color: rgba(54, 211, 154, 1)">Kedelai Kamila</h6>
+{{-- <script src=""></script> --}}
+</body>
+</html>
