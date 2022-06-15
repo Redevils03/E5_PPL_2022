f75f3d268cc490d3d6895fffcbfdf836d6318680
Artand <martinchandra99@gmail.com>
2022-06-15 23:40:23 +0700

    sistem selesai

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -38,11 +38,6 @@
                     {{ session('editEmpty') }}
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-                <script type="text/javascript">
-                    $(document).ready(function(){
-                        $("#tombol_edit").click(); 
-                    });
-                </script>
                 @php session()->forget('editEmpty') @endphp
             @endif
 
@@ -51,11 +46,6 @@
                     {{ session('beliEmpty') }}
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-                <script type="text/javascript">
-                    $(document).ready(function(){
-                        $("#tombol_beli").click(); 
-                    });
-                </script>
                 @php session()->forget('beliEmpty') @endphp
             @endif
         @endif
@@ -99,11 +89,12 @@
                     <ul class="dropdown-menu" role="menu" aria-labelledby="imageDropdown">
                         @if (Auth::guard('admin')->check())
                             <li role="presentation"><a role="menuitem" tabindex="-1" href="/daftarpembeli">Daftar Akun Pembeli</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="/daftarpembelian">Daftar Pembelian</a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="/pendapatan">Data Pendapatan</a></li>
                         @elseif (Auth::guard('web')->check())
                             <li role="presentation"><a role="menuitem" tabindex="-1" href="/chat/{{ Auth::id() }}">Chat Admin</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="/daftarpembelian">Daftar Pembelian</a></li>
                         @endif
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="/daftarpembelian">Daftar Pembelian</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="/infomitra">Informasi Mitra</a></li>
                     </ul>
                 </div>
             </div>
